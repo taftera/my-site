fe24b6e782d63e9f363390e697a08f4193445a63
Alejandro Turati <u01040101@iMac-de-Diseno.local>
2019-05-07 13:00:38 -0500

    Updating office files with home files

--- l-python/ex_003.py
+++ l-python/ex_003.py
@@ -1,24 +1,34 @@
-# Numbers and Math
+# Math & print
 
-print "I will now count my chickens:"
+print "I will know count my chickens:"
 
-print "Hens", 25 + 30 / 6
-print "Roosters", 100 - 25 * 3 % 4
-# % is a modulus - it gives out the remaining data from the division
+print "Hens", 25+30/6
+print "Roosters (100-25*3%4)", 100-25*3%4
+#% = Modulus > es el resto de la division > 100 % 16 = 6 > 100-96=4
 
-print "Now I will count the eggs:"
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print "Now I'll count the eggs:"
 
-print "Is it true that 3 + 2 < 5 - 7?"
+print 3+2+1-5+4%2-1/4+6
+
+print "Is it true that 3 + 2 < 5 - 7 ?"
 print 3 + 2 < 5 - 7
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7?", 5 - 7
-print "Oh, that's why it's False."
+
+print "What is 3 + 2 ?", 3+2
+print "What is 5 - 7 ?", 5-7
+
+print "Oh, that's why it's False"
 
 print "How about some more."
 
-print "Is it greater?", 5 > - 2
-print "Is it greater or equal?", 5 >= - 2
-print "Is it less or equal?", 5 <= - 2
+print "Is it greater?", 5>-2
+print "Is it greater or equal?", 5>=-2
+print "Is it less or equal?", 5<=-2
 
-# there's something about floating point
+# Order of operations
+# ====================
+# Parentheisis ()
+# Exponents
+# Multiplications
+# Divisions
+# Additions
+# Substractions
