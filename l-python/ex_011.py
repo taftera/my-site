0806071c3b2c9defd580fd605c8d9ad2bd66cfb4
Alejandro Turati <u01040101@iMac-de-Diseno.local>
2019-05-07 13:06:38 -0500

    Exercises made at the office 6 - 12 (21090506)

---
+++ l-python/ex_011.py
@@ -0,0 +1,13 @@
+#Asking Questions
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weight?",
+weight = raw_input()
+
+# By adding a " , " after each print, it allows to be answered in-line.
+# raw_input, presents a prompt to the user.
+
+print "So you're %r old, %r tall and %r heavy." % (age, height, weight)
