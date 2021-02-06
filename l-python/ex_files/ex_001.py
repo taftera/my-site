9ee4c85c4efe463f09fb9aeb39c749cb396b9cbf
Alex Turati <a.turati@gmail.com>
2021-02-06 12:56:44 -0600

    Delete l-python directory

--- l-python/ex_files/ex_001.py
+++
@@ -1,9 +0,0 @@
-#Just printing and #commenting
-
-print "Hello World!"
-# print "Hello Again"
-# print "I like typing this."
-# print "This is fun."
-# print 'Yay! Printing.'
-# print "I'd much rather you 'not'."
-# print 'I "said" do not touch this.'
